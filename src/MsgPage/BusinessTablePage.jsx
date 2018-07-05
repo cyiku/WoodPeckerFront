f5ee9a18bed914ca857e1b86d54f0c518e1bd234
lllwwwbbb <151220068@smail.nju.edu.cn>
2018-07-05 17:28:31 +0800

    add Business,Industry Page

---
+++ src/MsgPage/BusinessTablePage.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import {ShowTablePage} from "./ShowTablePage";
+import { connect } from 'react-redux';
+import { collectionActions } from '../_actions';
+import { Link } from 'react-router-dom';
+import { keywordActions } from '../_actions';
+// import { history } from '../_helpers';
+import { Popover } from 'antd';
+// import { openNotificationWithIcon } from "../_helpers";
+import {Button} from 'antd';
+// import { cmpTime } from '../_helpers';
+// import {errorProcess} from "../_helpers/error";
+// import {serverIP} from '../_helpers';
+
+class BusinessTablePage extends React.Component {
+
+    state = {
+        businessColumns: [
+            {title: '标题', dataIndex: 'title', width: "20%"},
+            {title: '正文', className: 'content', width: "40%", render: (record) => (
+                    <Popover content={
+                        <div style={{width: 400}}>
+                            <p dangerouslySetInnerHTML={{__html: this.markKeyword(record.content, record.keyword)}}/>
+                        </div>
+                    } title="全文内容">
+                        <p>{record.content}</p>
+                    </Popover>)},
+            {title: '来源', dataIndex: 'source'},
+            {title: '时间', dataIndex: 'time'},
+            {title: '关键字', dataIndex: 'keyword'},
+            {title: '原文', key: 'url', render: (record) => (<a href={record.url} target={"_blank"}>原文</a>)},
+        ],
+        currentKwd: '',
+    };
+
+    componentDidMount(){
+        //这里应该获取关键字对应的全部的收藏数据
+        const {user, dispatch, keyword} = this.props;
+        if (keyword === null)
+            dispatch(keywordActions.getKws(user));
+        if (this.props.collection['agency'] === null) {
+            dispatch(collectionActions.getCollection(user, 'agency'));
+        }
+    }
+
+    markKeyword = (content, keywords) => {
+        // 分割keywords
+        const keyword_list = keywords.split('_');
+        //console.log(keyword_list);
+        for (let i = 0; i < keyword_list.length; ++i) {
+            //content.replace(keyword_list[i], '<span style="color: red">'+keyword_list[i]+'</span>')
+            content = content.replace(new RegExp(keyword_list[i], "gm"), '<span style="color: red">'+keyword_list[i]+'</span>');
+        }
+        //console.log(content);
+        return content;
+    };
+
+    clickKeyword = (event) => {
+        let newKwd = event.target.getAttribute("value");
+        if (newKwd === this.state.currentKwd || newKwd === null)
+            return;
+
+        let targets = document.getElementsByClassName("keyword");
+
+        for (let i = 0; i < targets.length; ++i) {
+            targets[i].setAttribute("type", "default");
+        }
+        event.target.setAttribute("type", "primary");
+
+        this.setState(preState => ({
+            ...preState,
+            currentKwd: newKwd,
+            agencyData: null,
+        }));
+
+    };
+
+    render() {
+
+        let {keyword} = this.props;
+        if (keyword === null)
+            return <div>请求中, 请稍候</div>;
+
+        let currentKwd = '';
+        if(this.state.currentKwd === '') {
+            if (keyword.length > 0) {
+                currentKwd = keyword[0].name;
+            }
+        } else {
+            currentKwd = this.state.currentKwd;
+        }
+
+        if (currentKwd === '')
+            return <div>暂无关键字</div>;
+
+        let kwdButtonClass = {};
+        for (let i = 0; i < keyword.length; i++) {
+            if (keyword[i].name === currentKwd) {
+                kwdButtonClass[keyword[i].name] = "primary";
+            } else {
+                kwdButtonClass[keyword[i].name] = "default";
+            }
+        }
+
+        const type = "agency";
+        const columns = this.state.businessColumns;
+        const title = " 商务资讯";
+        const collection = this.props.collection['agency'];
+        return (
+            <div style={{marginLeft:15, marginTop:15}}>
+                <div>
+                    {
+                        keyword.map( (oneKwd, index)=>
+                            <Button
+                                size="large"
+                                type={kwdButtonClass[oneKwd.name]}
+                                key={index}
+                                onClick={this.clickKeyword}
+                                value={oneKwd.name}
+                                style={{marginRight:15}}
+                            >
+                                {oneKwd.name}
+                            </Button>
+                        )
+                    }
+                    <Button type="primary" size="large"><Link to="/keywords">管理关键字</Link></Button>
+                </div>
+                <div style={{marginTop:15}}>
+                    <ShowTablePage columns={columns} type={type} title={title} collection={collection} keyword={currentKwd}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+function mapStateToProps(state) {
+    const { authentication, collection, keyword } = state;
+    const { user } = authentication;
+    return {
+        user, collection, keyword
+    };
+}
+
+const connectedBusinessTablePage = connect(mapStateToProps)(BusinessTablePage);
+export { connectedBusinessTablePage as BusinessTablePage };
+
