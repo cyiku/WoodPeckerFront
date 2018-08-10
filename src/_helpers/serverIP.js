648519e5f122d58d7fc0f534da64bf9f23ba4e16
欧阳亚文 <550155036@qq.com>
2018-08-10 09:41:30 +0800

    rebuild

--- src/_helpers/serverIP.js
+++ src/_helpers/serverIP.js
@@ -1,4 +1,4 @@
-const serverIP = "http://114.212.189.147:10107";
-//const serverIP = "http://localhost:8080";
+//const serverIP = "http://114.212.189.147:10107";
+const serverIP = "http://localhost:8080";
 
 export {serverIP};
